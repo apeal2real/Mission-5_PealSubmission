aba653a36d10c124a640ac51516fcecd4aceb2ef
apeal2real <alec.peal@gmail.com>
2024-02-07 20:39:35 -0700

    Add project files.

---
+++ Mission#5iLoveDrums/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#calculate-btn').click(function () {
+        var hours = parseFloat($('#hours').val());
+        var hourlyRate = parseFloat($('#hourly-rate').val().replace('$', ''));
+
+        if (isNaN(hours) || hours <= 0) {
+            alert('Please enter a valid number of hours.');
+            return;
+        }
+
+        var total = hours * hourlyRate;
+        $('#total').val('$' + total.toFixed(2));
+    });
+});
+
